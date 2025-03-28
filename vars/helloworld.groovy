e6a56ede08985e75e5880cd2e5d27ec3c4b4b1bf
indugone <indugone411@gmail.com>
2025-03-28 14:38:14 +0530

    Update helloworld.groovy

--- vars/helloworld.groovy
+++ vars/helloworld.groovy
@@ -1,5 +1,3 @@
-def demo(name) {
-    echo "my name is indu $name"  // Corrected string interpolation
-    git 'https://github.com/indugone/sharedlibrary.git'  // Cloning the Git repository
-    sh "ls"  // Listing files in the workspace
+def call() {
+    echo "my name is indu $name"  // Corrected string interpolatio
 }
